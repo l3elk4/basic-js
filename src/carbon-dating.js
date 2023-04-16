b08718526af1af246e76e4199cd598fc40da2c02
l3elk4 <119819542+l3elk4@users.noreply.github.com>
2023-04-17 00:54:17 +0300

    fix: carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -19,11 +19,11 @@ const HALF_LIFE_PERIOD = 5730;
  */
 function dateSample(n) {
   const modernActivity = 15; 
-  const halfLifePeriod = 5370;
-  let k = 0.693/halfLifePeriod;
-  if (typeof (n) !== 'string') {return false}
-  let t = (Math.log(modernActivity/n))/k;
-  if (0<=n<=15) {return (Math.ceil(t))} 
+  const halfLifePeriod = 5730;
+  if(typeof n !== 'string') return false
+  k = Math.log(2)/halfLifePeriod;
+  t = (Math.log(modernActivity/n))/k;
+  if (n>0 && n<=15) {return (Math.ceil(t))} 
   else return false
 }
 module.exports = {
