e749ff10aaa0ed73f1878a6bad11d2ed54c39e48
l3elk4 <119819542+l3elk4@users.noreply.github.com>
2023-04-18 01:54:03 +0300

    feat: add repeater and sum digits tasks

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,10 +15,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+function repeater(str, options ) {
+  if (options.separator === undefined){
+    options.separator = '+';
+  }
+  if (options.additionSeparator === undefined){
+    options.additionSeparator = '|';
+  }
+  const repeatFunc = (separator, string = '', repeatTimes = 1) =>{
+    let result = [];
+    for (let i=0; i<repeatTimes;i++){
+      result.push(String(string));
+    }return result.join(separator)
+  }
+  const fullAddition = repeatFunc(options.additionSeparator, options.addition, options.additionRepeatTimes);
+  options.separator = fullAddition + options.separator;
+  return repeatFunc(options.separator, str, options.repeatTimes) + fullAddition;
+  }
 
 module.exports = {
   repeater
--- src/sum-digits.js
+++ src/sum-digits.js
@@ -12,9 +12,10 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSumOfDigits(n) {
+  let arr = String(n).split('');
+  let result = arr.reduce((accum, el) => accum + Number(el), 0);
+  return String(result).length === 1 ? result : getSumOfDigits(result);
 }
 
 module.exports = {
